6226096c73f0a0ea0f6aac720d74da2ad4773359
PC <nachotafalla@gmail.com>
2026-03-16 00:36:39 +0100

    arreglando search

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -6,9 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/search")
+@app.route("/search", methods=["GET"])
 def search():
-    return render_template("search.html")
+    query = request.args.get("query")
+    response = requests.get("https://api.tvmaze.com/search/shows?", params={"q": query})
+    data = response.json()
+    print(type(data))
+    print(data)
+    return render_template("search.html",results=data)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
