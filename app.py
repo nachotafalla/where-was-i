ad225857d8ab2d04a654454079a2c61b9dcfa3dd
PC <nachotafalla@gmail.com>
2026-03-19 19:04:54 +0100

    search terminado

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ def search():
     query = request.args.get("query")
     response = requests.get("https://api.tvmaze.com/search/shows?", params={"q": query})
     data = response.json()
-    print(type(data))
-    print(data)
     return render_template("search.html",results=data)
     
+@app.route("/details", methods=["GET"])
+def details():
+
 
 if __name__ == "__main__":
     app.run(debug=True)
